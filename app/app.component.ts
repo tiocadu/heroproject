a087edceb2eac9b1b5db4952c4dc4003ae73020a
Eduardo Rocha <edugaspar@hotmail.com>
2016-06-01 19:46:23 -0300

    The hero editor. One-way and two-way data binding usind double brackets and ngModel.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,32 @@
 import {Component} from '@angular/core';
 
+
+
+export class Hero {
+    id: number;
+    name: string;
+}
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><p>My name is {{myname}}!</p>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+    `
 })
 export class AppComponent { 
 
-    private myname = 'Cadu';
+    private title = 'Tour of Heroes';
+    
+    private hero: Hero {
+        id: 1,
+        name: 'Windstorm'
+    };
 
 }
