e0536982ec4c6877e42f638d8cd0f637c727ab51
Eduardo Rocha <edugaspar@hotmail.com>
2016-06-02 00:36:50 -0300

    Added getHeroesSlowly in the service to simulate a late response from the server.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 
+import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 
 
@@ -8,5 +9,11 @@ export class HeroService {
 
     public getHeroes() {
         return Promise.resolve(HEROES);
+    }
+
+    public getHeroSlowly() {
+        return new Promise<Hero[]>( resolve =>
+            setTimeout(()=>resolve(HEROES), 2000)
+        );
     }
 }
